badc33ebc7a73c72f1e4356d70d17e6013681572
Gabriel Cánepa <gacanepa@gmail.com>
2022-06-25 20:06:31 -0300

    Configured CORS and added env variables

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// Constants
-const PORT = 8000;
-
 // Import tools
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
+const cors = require('cors');
 
 // Load environment file
 require('dotenv').config()
+
+// Get port from .env file
+const PORT = process.env.API_PORT
 
 // Get connection string from the environment file
 const dbConnectionString = process.env.DB_CONNECTION_STRING;
@@ -27,6 +28,9 @@ database.once('connected', () => {
 
 // Initialize app and routes
 const app = express();
+app.use(cors({
+  origin: process.env.FRONTEND_APP,
+}));
 app.use(express.json());
 app.use('/api', routes);
 app.listen(PORT, () => {
